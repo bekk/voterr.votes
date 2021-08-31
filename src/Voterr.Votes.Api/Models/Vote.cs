c401c7f75123119f35039c1217bfded3f351db5e
Anders Refsdal Olsen <andersro93@ros-nett.com>
2021-08-31 22:30:02 +0200

    Added serialization names

--- src/Voterr.Votes.Api/Models/Vote.cs
+++ src/Voterr.Votes.Api/Models/Vote.cs
@@ -1,11 +1,19 @@
-using System;
+using Newtonsoft.Json;
 
 namespace Voterr.Votes.Api.Models
 {
     public class Vote
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("candidateId")]
         public int CandidateId { get; set; }
+        
+        [JsonProperty("voterObjectId")]
         public string VoterObjectId { get; set; }
+        
+        [JsonProperty("voterTenantId")]
         public string VoterTenantId { get; set; }
     }
 }
