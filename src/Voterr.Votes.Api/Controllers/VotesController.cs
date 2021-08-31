0c419748c4b6f7b3651a1f65c77945132882df15
Anders Refsdal Olsen <andersro93@ros-nett.com>
2021-08-31 19:12:39 +0200

    Removed unused template stuff

---
+++ src/Voterr.Votes.Api/Controllers/VotesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Voterr.Votes.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/votes")]
+    public class VotesController : ControllerBase
+    {
+        private readonly ILogger<VotesController> _logger;
+
+        public VotesController(ILogger<VotesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new{ };
+        }
+    }
+}
