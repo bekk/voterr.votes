d8f5340bac668fc2b10d33dd67f2865fa8513ca2
Espen Ekvang <espen.ekvang@bekk.no>
2021-09-01 16:03:38 +0200

    Move featuremanagement to startup

--- src/Voterr.Votes.Api/Startup.cs
+++ src/Voterr.Votes.Api/Startup.cs
@@ -1,11 +1,14 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Identity.Web;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.FeatureManagement;
+using Voterr.Votes.Api.Extensions;
 using Voterr.Votes.Api.Services;
 
 namespace Voterr.Votes.Api
@@ -23,12 +26,22 @@ namespace Voterr.Votes.Api
         {
             services.AddApplicationInsightsTelemetry();
             services.AddFeatureManagement();
-            
+
             services
                 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
             
-            services.AddSingleton<CosmosDbClient>();
+            services.AddSingleton(sp =>
+            {
+	            var featureManager = sp.GetRequiredService<IFeatureManager>();
+
+	            if (featureManager.IsEnabled(FeatureFlags.UseManagedIdentity))
+	            {
+		            var cosmosUri = Configuration["CosmosUri"];
+		            return new CosmosClient(cosmosUri, new DefaultAzureCredential());
+	            }
+	            return new CosmosClient(Configuration.GetConnectionString("Cosmos"));
+            });
 
             services.AddTransient<VotesService>();
             
